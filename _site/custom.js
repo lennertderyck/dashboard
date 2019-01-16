3ca6a56496b41c4e085a4790be29c771c0ff3832
Lennert De Ryck <admin@lennertderyck.be>
2019-01-16 20:48:45 +0000

    Update from Forestry.io

--- _site/custom.js
+++
@@ -1,9 +0,0 @@
-function openOptions() {
-    var element = document.getElementById("container-notif-overlay");
-    element.classList.add("toggle-active");
-}
-
-function closeOptions() {
-    var element = document.getElementById("container-notif-overlay");
-    element.classList.remove("toggle-active");
-}
