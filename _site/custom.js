c66aa9d6207aebd0f89181a72aeb57d75e646c22
gdm-1718-lenndery <32176355+gdm-1718-lenndery@users.noreply.github.com>
2019-04-30 18:13:16 +0200

    Update
    Added support for public transport outcome links (delijn.be)
    
    Added Bootstrap

---
+++ _site/custom.js
@@ -0,0 +1,9 @@
+function openOptions() {
+    var element = document.getElementById("container-notif-overlay");
+    element.classList.add("toggle-active");
+}
+
+function closeOptions() {
+    var element = document.getElementById("container-notif-overlay");
+    element.classList.remove("toggle-active");
+}
